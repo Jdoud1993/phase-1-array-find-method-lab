5c354aee48ea49994c5190bb2f4f40781b201262
Joshua Doud <bigjj3393@yahoo.com>
2022-10-19 12:17:35 -0700

    commit tree b8ba6d2dd8982d949dd208fd99c5944361f77433

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array) {
+    let win = array.find(isWin)
+    if (win) {
+      return win.year
+    }
+    return win
+  }
+  
+  function isWin(score){
+      return score.result === "W"
+        
+  }
